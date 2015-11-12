db27913956695b135d8a6da544c3e52fec271597
Demo User <debian@beaglebone.(none)>
2015-11-12 19:30:46 +0000

    Automatically add udev rule for nonroot access to /dev/pinpirate

--- term-proj/scripts/start-ide.sh
+++ term-proj/scripts/start-ide.sh
@@ -6,6 +6,9 @@ cd $CURRENT_DIR
 
 sudo ./activate-pruss.sh
 sudo ./install-BBB-kernel-headers.sh
+if [ ! -e "/etc/udev/rules.d/99-pinpirate.rules" ]; then
+	sudo cp ../driver/99-pinpirate.rules /etc/udev/rules.d/	
+fi
 if ! lsmod | grep --quiet pin_pirate; then
 	echo pin-pirate LKM not loaded 
 	pushd .
